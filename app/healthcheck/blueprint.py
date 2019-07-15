454e3087698f91c826e72f5b2f012d2c8c73aec7
Bianca Rosa <me@biancarosa.com.br>
2019-07-14 21:05:11 -0300

    use / route

--- app/healthcheck/blueprint.py
+++ app/healthcheck/blueprint.py
@@ -5,5 +5,5 @@ from app.healthcheck import healthcheck
 def create_blueprint():
     """Creates a Blueprint"""
     blueprint = Blueprint('Health Check Blueprint', __name__)
-    blueprint.route('/health-check')(healthcheck.healthcheck)
+    blueprint.route('/')(healthcheck.healthcheck)
     return blueprint
